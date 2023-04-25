3f3becba25389007569e18a1881808f89ec8fce5
Duc Do <duc.fet01@gmail.com>
2023-04-25 17:23:43 +0300

    Fix test

--- Tests/SauronTests/SauronTests.swift
+++ Tests/SauronTests/SauronTests.swift
@@ -2,10 +2,8 @@ import XCTest
 @testable import Sauron
 
 final class SauronTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Sauron().logMode, .enable)
+    func testDefaultMode() throws {
+        // Default mode shall always disable
+        XCTAssertEqual(Sauron().logMode, .disable)
     }
 }
